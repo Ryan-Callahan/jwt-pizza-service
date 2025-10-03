f7abfaf8debb30151a244c7a8d45cb354c5e1b7e
Ryan Callahan <ryan.callahan1124@gmail.com>
2025-10-03 15:36:53 -0600

    excluded tests from the linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,10 @@
 import js from "@eslint/js";
+import jest from 'eslint-plugin-jest';
 import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { files: ["**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts}"], plugins: { jest }, rules: { ...jest.configs.recommended.rules }, languageOptions: { globals: { ...globals.jest } } }
 ]);
--- src/service.test.js
+++ src/service.test.js
@@ -1,5 +1,4 @@
-const request = require('supertest');
-const app = require('./service');
+
 
 test('hello world', () => {
     expect(true).toBe(true);
