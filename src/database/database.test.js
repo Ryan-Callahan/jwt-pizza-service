7070f52a790b2a368e73dbf68d7fbb2068bb516f
Ryan Callahan <ryan.callahan1124@gmail.com>
2025-10-06 15:14:07 -0600

    added one simple test

---
+++ src/database/database.test.js
@@ -0,0 +1,7 @@
+const { DB } = require("./database")
+
+test("add user", async () => {
+    DB.addUser({name: "test-user", password: "test-password", email: "test-email"})
+    const actualUser = await DB.getUser("test-email", "test-password")
+    expect(actualUser.name).toEqual("test-user")
+})
