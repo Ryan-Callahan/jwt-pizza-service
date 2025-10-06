377e3885aef02eed2821c3a7740ed2680eb8abbf
Ryan Callahan <ryan.callahan1124@gmail.com>
2025-10-06 15:17:39 -0600

    fixed that test hopefully

--- src/database/database.test.js
+++ src/database/database.test.js
@@ -1,7 +1,7 @@
 const { DB } = require("./database")
 
 test("add user", async () => {
-    DB.addUser({name: "test-user", password: "test-password", email: "test-email"})
+    DB.addUser({name: "test-user", password: "test-password", email: "test-email", roles: []})
     const actualUser = await DB.getUser("test-email", "test-password")
     expect(actualUser.name).toEqual("test-user")
 })
