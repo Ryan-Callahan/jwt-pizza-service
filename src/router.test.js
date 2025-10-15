ef9a03b48b8c1a8815549ff172afa3b3f343b209
Ryan Callahan <ryan.callahan1124@gmail.com>
2025-10-15 13:54:54 -0600

    Wrote the backend unit tests for listUsers endpoint, stubbed out the endpoint too

--- src/router.test.js
+++ src/router.test.js
@@ -51,6 +51,35 @@ test("updating a user should work", async () => {
     expect(res.status).toBe(200);
 });
 
+test('list users unauthorized', async () => {
+  const listUsersRes = await request(app).get('/api/user');
+  expect(listUsersRes.status).toBe(401);
+});
+
+test('list users', async () => {
+  const [user, userToken] = await registerUser(request(app));
+  const listUsersRes = await request(app)
+    .get('/api/user')
+    .set('Authorization', 'Bearer ' + userToken);
+  expect(listUsersRes.status).toBe(200);
+});
+
+async function registerUser(service) {
+  const testUser = {
+    name: 'pizza diner',
+    email: `${randomName()}@test.com`,
+    password: 'a',
+  };
+  const registerRes = await service.post('/api/auth').send(testUser);
+  registerRes.body.user.password = testUser.password;
+
+  return [registerRes.body.user, registerRes.body.token];
+}
+
+function randomName() {
+  return Math.random().toString(36).substring(2, 12);
+}
+
 // franchiseRouter tests
 test("get franchises should work", async () => {
     const res = await request(app).get('/api/franchise/');
