f39c861e3c739af0e03435daa2b2caf3db0f9b4a
Ryan Callahan <ryan.callahan1124@gmail.com>
2025-11-07 16:00:48 -0700

    tryign to downgrade to see if thatg works i hate this

--- src/metrics.js
+++ src/metrics.js
@@ -1,11 +1,8 @@
 const config = require('./config');
-const os = require('os');
-
-const PIZZA_FACTORY_ENDPOINT = `${config.factory.url}/api/order`
 
 // Metrics stored in memory
 const requests = {};
-const pizzaPurchaseStats = {};
+
 
 // Middleware to track requests
 function requestTracker(req, res, next) {
@@ -14,67 +11,15 @@ function requestTracker(req, res, next) {
   next();
 }
 
-function getCpuUsagePercentage() {
-  const cpuUsage = os.loadavg()[0] / os.cpus().length;
-  return cpuUsage.toFixed(2) * 100;
-}
-
-function getMemoryUsagePercentage() {
-  const totalMemory = os.totalmem();
-  const freeMemory = os.freemem();
-  const usedMemory = totalMemory - freeMemory;
-  const memoryUsage = (usedMemory / totalMemory) * 100;
-  return memoryUsage.toFixed(2);
-}
-
-function pizzaPurchase(status, latencyMs, order) {
-  if (!pizzaPurchaseStats[status]) {
-    pizzaPurchaseStats[status] = {
-      count: 0,
-      totalLatencyMs: 0,
-      totalPrice: 0,
-      totalPizzas: 0,
-    };
-  }
-
-  const pizzas = Array.isArray(order?.items) ? order.items : [];
-  const pizzaCount = pizzas.length;
-  const orderPrice = pizzas.reduce((sum, pizza) => {
-    return sum + ((typeof pizza.price === 'number') ? pizza.price : 0);
-  }, 0);
-
-  const stat = pizzaPurchaseStats[status];
-  stat.count += 1;
-  stat.totalLatencyMs += latencyMs;
-  stat.totalPrice += orderPrice;
-  stat.totalPizzas += pizzaCount
-}
-
 // This will periodically send metrics to Grafana
-if (process.env.NODE_ENV !== 'test') {
-  setInterval(() => {
-    const metrics = [];
-    Object.keys(requests).forEach((endpoint) => {
-      metrics.push(createMetric('requests', requests[endpoint], '1', 'sum', 'asInt', {endpoint}));
-    });
+setInterval(() => {
+  const metrics = [];
+  Object.keys(requests).forEach((endpoint) => {
+    metrics.push(createMetric('requests', requests[endpoint], '1', 'sum', 'asInt', { endpoint }));
+  });
 
-    Object.keys(pizzaPurchaseStats).forEach((status) => {
-      const stat = pizzaPurchaseStats[status];
-      const attributes = {status, endpoint: PIZZA_FACTORY_ENDPOINT}
-
-      metrics.push(createMetric('pizza_purchase_requests', stat.count, '1', 'sum', 'asInt', attributes))
-      metrics.push(createMetric('pizza_purchase_latency_total', stat.totalLatencyMs, 'ms', 'sum', 'asDouble', attributes))
-      metrics.push(createMetric('pizza_purchase_price_total', stat.totalPrice, '1', 'sum', 'asDouble', attributes))
-      metrics.push(createMetric('pizza_purchase_pizza_total', stat.totalPizzas, '1', 'sum', 'asInt', attributes))
-
-    })
-
-    metrics.push(createMetric('cpu', getCpuUsagePercentage(), '%', 'gauge', 'asInt', {}))
-    metrics.push(createMetric('memory', getMemoryUsagePercentage(), '%', 'gauge', 'asInt', {}))
-
-    sendMetricToGrafana(metrics);
-  }, 10000);
-}
+  sendMetricToGrafana(metrics);
+}, 10000);
 
 function createMetric(metricName, metricValue, metricUnit, metricType, valueType, attributes) {
   attributes = { ...attributes, source: config.metrics.source };
@@ -109,19 +54,9 @@ function createMetric(metricName, metricValue, metricUnit, metricType, valueType
 }
 
 function sendMetricToGrafana(metrics) {
-
-  if (process.env.NODE_ENV === 'test') {
-    return;
-  }
-
   const body = {
     resourceMetrics: [
       {
-        resource: {
-          attributes: [
-            { key: 'service.name', value: { stringValue: config.metrics.source }}
-          ],
-        },
         scopeMetrics: [
           {
             metrics,
@@ -146,4 +81,4 @@ function sendMetricToGrafana(metrics) {
     });
 }
 
-module.exports = { requestTracker, pizzaPurchase };
+module.exports = { requestTracker };
